51e61749dba58108e3d4f82179b4ba5e2037c664
KristofferSandas <113130284+KristofferSandas@users.noreply.github.com>
2023-06-26 12:10:49 +0200

    Update pathogen_df.py

--- nanometa_live/gui_scripts/pathogen_df.py
+++ nanometa_live/gui_scripts/pathogen_df.py
@@ -1,13 +1,11 @@
-"""
-Creates a df of data on specified pathogens from config list.
-
-
-"""
-
 import pandas as pd
 import math
 
 def pathogen_df(pathogen_list, raw_df): 
+    """
+    Creates a df of data on specified pathogens from config list.
+    """
+    
     # df makes layout much easier
     pathogen_info = pd.DataFrame(columns= ['Name', 
                                            'Tax ID',
@@ -25,7 +23,7 @@ def pathogen_df(pathogen_list, raw_df):
                     log10reads = 0 # set it to 0
                 else: # get the log of the reads for danger meter
                     log10reads = math.log(raw_df.iloc[i,2],10)
-                # add the species to the results df. Fulkod.
+                # add the species to the results df.
                 pathogen_info.loc[len(pathogen_info.index)] = [raw_df.iloc[i,5], # add pathogen name 
                                                                raw_df.iloc[i,4], # add pathogen taxID
                                                                raw_df.iloc[i,2], # add pathogen nr of reads
