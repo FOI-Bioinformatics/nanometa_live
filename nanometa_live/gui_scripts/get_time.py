87c860a0c92ed335b0197c253160fb85d30a6061
KristofferSandas <113130284+KristofferSandas@users.noreply.github.com>
2023-06-26 12:07:18 +0200

    Update get_time.py

--- nanometa_live/gui_scripts/get_time.py
+++ nanometa_live/gui_scripts/get_time.py
@@ -1,11 +1,9 @@
-"""
-Returns the current time.
-
-"""
-
 import time
 
 def get_time():
+    """
+    Returns the current time.
+    """
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     return current_time
