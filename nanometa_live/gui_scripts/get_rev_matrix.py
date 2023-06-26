dfb144d18704c8ebe7651b4574b4d985f5c6fbe1
KristofferSandas <113130284+KristofferSandas@users.noreply.github.com>
2023-06-26 12:06:41 +0200

    Update get_rev_matrix.py

--- nanometa_live/gui_scripts/get_rev_matrix.py
+++ nanometa_live/gui_scripts/get_rev_matrix.py
@@ -1,15 +1,12 @@
-
-"""
-Creates a reversed matrix from the domain filtered df
-in order to parse through the list 
-starting with lowest tax level and assigning it to the corresponding 
-parent clade.
-
-"""
-
 import numpy as np
 
 def get_rev_matrix(d_filt_df): 
+    """
+    Creates a reversed matrix from the domain filtered df
+    in order to parse through the list 
+    starting with lowest tax level and assigning it to the corresponding 
+    parent clade.
+    """
     rev_df = d_filt_df.iloc[::-1] # reverses the df
     # adds the data needed
     names = rev_df[rev_df.columns[5]].values.tolist()
