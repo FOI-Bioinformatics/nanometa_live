016cb9fe899352d145ccb1f71cc3785fc30c1199
KristofferSandas <113130284+KristofferSandas@users.noreply.github.com>
2023-06-26 12:09:59 +0200

    Update kreport2_df.py

--- nanometa_live/gui_scripts/kreport2_df.py
+++ nanometa_live/gui_scripts/kreport2_df.py
@@ -1,12 +1,9 @@
-
-"""
-Imports kreport2 file and creates a pd dataframe.
-
-"""
-
 import pandas as pd
 
 def kreport2_df(kreport_file):
+    """
+    Imports kreport2 file and creates a pd dataframe.
+    """
     raw_kraken_df = pd.read_csv(kreport_file,
                                 sep = '\t',
                                 # removes spaces in col 5
