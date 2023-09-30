588d2a5ad5240aaca32a9f112d7b75a62c1bbf8f
Andreas Sjödin <andreas.sjodin@gmail.com>
2023-09-30 07:30:52 +0200

    Refactor blast

--- nanometa_live/helpers/blast_utils.py
+++ nanometa_live/helpers/blast_utils.py
@@ -3,19 +3,22 @@ import logging
 import subprocess
 from typing import Any, List, Dict, Union, NoReturn, List
 
-def build_blast_databases(workdir: str) -> NoReturn:
+def build_blast_databases(workdir: str, missing_databases: List[str] = None) -> NoReturn:
     """
     Build BLAST databases for each reference sequence in the genomes folder
-    located in the working directory.
+    located in the working directory. Only builds databases for genomes
+    that are in the missing_databases list.
 
     Parameters:
         workdir (str): Path to the working directory.
+        missing_databases (List[str]): List of missing BLAST databases by Tax ID.
 
     Raises:
         Exception: Any exception that occurs during the database build process.
     """
     try:
         input_folder = os.path.join(workdir, "genomes")
+        blast_db_folder = os.path.join(workdir, "blast")
 
         if not os.path.exists(input_folder):
             logging.error(f"Input folder {input_folder} does not exist. Exiting.")
@@ -27,13 +30,25 @@ def build_blast_databases(workdir: str) -> NoReturn:
             logging.warning(f"No files found in {input_folder}. Nothing to process.")
             return
 
+        if not missing_databases:
+            logging.info("No missing databases. Skipping BLAST database building.")
+            return
+
         logging.info(f"Found {len(files_to_process)} files to process.")
 
         for file in files_to_process:
+            # Extract taxid from the file name
+            taxid = os.path.splitext(file)[0]
+
+            # Check if BLAST database needs to be built for this genome
+            if missing_databases and taxid not in missing_databases:
+                logging.info(f"BLAST database already exists for Tax ID: {taxid}. Skipping.")
+                continue
+
             file_path = os.path.join(input_folder, file)
             logging.info(f"Processing file: {file_path}")
 
-            database_name = os.path.join(workdir, "blast", file)
+            database_name = os.path.join(blast_db_folder, file)
             system_cmd = ["makeblastdb", "-in", file_path, "-dbtype", "nucl", "-out", database_name]
 
             # Create a database for the reference sequence using BLAST
@@ -49,3 +64,41 @@ def build_blast_databases(workdir: str) -> NoReturn:
         logging.error(f"An unexpected error occurred: {e}")
         raise
 
+def check_blast_dbs_exist(species_to_taxid: Dict[str, int], data_files_folder: str) -> List[str]:
+    """
+    Parameters:
+        species_to_taxid (Dict[str, int]): Dictionary mapping species names to tax IDs.
+        data_files_folder (str): Path to the folder containing the BLAST databases.
+
+    Returns:
+        List[str]: List of missing databases by taxid.
+    """
+
+    logging.info("Starting to check existence of BLAST databases.")
+    data_files_folder = os.path.join(data_files_folder, "blast")
+
+    missing_dbs = []
+
+    if not species_to_taxid:
+        logging.warning("Received an empty species to taxid map. No BLAST databases to check.")
+        return missing_dbs
+
+    if not os.path.exists(data_files_folder):
+        logging.error(f"Data files folder '{data_files_folder}' does not exist.")
+        return missing_dbs
+
+    for species, taxid in species_to_taxid.items():
+        blast_db_file = os.path.join(data_files_folder, f"{taxid}.fasta.nhr")
+
+        if not os.path.exists(blast_db_file):
+            logging.info(f"BLAST database for {species} (Tax ID: {taxid}) does not exist.")
+            missing_dbs.append(str(taxid))
+        else:
+            logging.info(f"BLAST database for {species} (Tax ID: {taxid}) exists.")
+
+    if missing_dbs:
+        logging.warning(f"Missing BLAST databases for Tax IDs: {', '.join(missing_dbs)}")
+    else:
+        logging.info("All BLAST databases exist for the given species list.")
+
+    return missing_dbs
